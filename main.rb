35acd83ddc77e64f886892eb6d58d33a3f192ebd
Alex <alex.zneue@gmail.com>
2017-06-22 19:11:25 +0300

    'Bear'-minimum ruby+blt setup

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
-require './src/game'
-Game.new
+
+require './lib/BearLibTerminal'
+
+Terminal.open()
+
+Terminal.print(1, 1, 'Hello World!')
+Terminal.refresh()
+
+while Terminal.read() != Terminal::TK_CLOSE
+end
+
+Terminal.close()

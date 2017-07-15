c007e68ee83de6367fe8f7ee6a23d37c7be26831
Alex <alex.zneue@gmail.com>
2017-07-15 09:59:08 +0300

    Add tile statuses

--- src/world/map.rb
+++ src/world/map.rb
@@ -10,15 +10,24 @@ module Map
     @width  = opts.width
     @height = opts.height
 
-    define_tile_types TILES.tile_types
+    define_tile_types TILES.tile_types, TILES.tile_statuses
 
     generate
   end
 
 
-  def self.set_tile x, y, type
+  def self.set_tile x, y, type, status = :normal
+    type = "#{status}_#{type}".to_sym
     variant = @types[type].sample
     @tiles[x][y] = variant
+  end
+
+
+  def self.set_status x, y, status
+    return if out_of_bounds? x, y
+
+    type = @tiles[x][y].to_s[@tiles[x][y].to_s.index('_')..-1]
+    @tiles[x][y] = "#{status}#{type}".to_sym
   end
 
 
@@ -51,60 +60,72 @@ module Map
   end
 
 
-  internal def self.define_tile_types tiles
+  internal def self.define_tile_types tiles, statuses
     default_color = '255 255 255'
 
     @types = {}
 
     tiles.each do |key, type|
+      statuses.each do |status, overrides|
 
-      key    = key.to_sym
-      chars  = [ type.char ].flatten
-      colors = define_tile_colors type.color || default_color
-      can = {
-        walk:  type.can&.include?('walk'),
-        fly:   type.can&.include?('fly'),
-        see:   type.can&.include?('see')
-      }
-      count = 0
-      @types[key] = []
+        status_key = "#{ status }_#{ key }".to_sym
+        count = 0
+        @types[status_key] = []
 
-      chars.each do |char|
-        colors.each do |color|
-          variant = "#{ key }_#{ count }".to_sym
+        chars  = [ overrides&.char || type.char ].flatten
+        colors = define_tile_colors overrides&.color || type.color || default_color
+        can    = define_tile_can overrides&.can || type.can
 
-          @types[key] << variant
-          @types[variant] = {
-            char:  char,
-            color: color,
-            walk:  can.walk,
-            fly:   can.fly,
-            see:   can.see
-          }
+        chars.each do |char|
+          colors.each do |color|
+            variant = "#{ status_key }_#{ count }".to_sym
 
-          count += 1
+            @types[status_key] << variant
+            @types[variant] = {
+              char:  char,
+              color: color,
+              walk:  can.walk,
+              fly:   can.fly,
+              see:   can.see
+            }
+
+            count += 1
+          end
         end
-      end
 
+      end
     end
   end
 
 
   internal def self.define_tile_colors colors
     [ colors ].flatten.inject([]) do |result, color|
-      r, g, b = 255, 255, 255
+      r, g, b, x = 'ff', 'ff', 'ff', '80'
 
       if color.is_a? String
-        r, g, b = color.split(' ').map do |c|
-          c.to_i.to_s(16).rjust 2, '0'
-        end
+        r, g, b = *color.split(' ').map(&:to_i)
+        x = (r + g + b) / 12
+
+        r = r.to_s(16).rjust 2, '0'
+        g = g.to_s(16).rjust 2, '0'
+        b = b.to_s(16).rjust 2, '0'
+        x = x.to_s(16).rjust 2, '0'
       end
 
       result << {
         full: "ff#{r}#{g}#{b}",
-        half: "30#{r}#{g}#{b}"
+        half: "ff#{x}#{x}#{x}"
       }
     end
+  end
+
+
+  internal def self.define_tile_can can
+    {
+      walk: can&.include?('walk'),
+      fly:  can&.include?('fly'),
+      see:  can&.include?('see')
+    }
   end
 
 
